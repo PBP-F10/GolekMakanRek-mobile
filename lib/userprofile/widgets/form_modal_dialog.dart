f2d43554b9f675b229638e4a83226dfff02bacb7
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2024-12-22 19:13:58 +0700

    fixing userprofile

--- lib/userprofile/widgets/form_modal_dialog.dart
+++ lib/userprofile/widgets/form_modal_dialog.dart
@@ -395,7 +395,7 @@ class _FormModalDialogState extends State<FormModalDialog> {
                                 onPressed: () async {
                                   if (_formKey.currentState!.validate()) {
                                       print(_dateOfBirthController.text);
-                                      final response = await request.postJson(
+                                      final response = await request.post(
                                         "https://joshua-montolalu-golekmakanrek.pbp.cs.ui.ac.id/userprofile/userprofile/update-flutter",
                                         jsonEncode(<String, dynamic>{
                                           'description': _description,
